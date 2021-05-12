0e93758be031234a000589d674c4cc9d84f820f6
Peter Macaluso <moxious.manoeuvre@gmail.com>
2021-05-12 14:36:53 -0400

    replace in app

--- application.py
+++ application.py
@@ -6,7 +6,7 @@ app = Flask(__name__)
 import os
 from dotenv import load_dotenv
 load_dotenv()
-app.config['SQLALCHEMY_DATABASE_URI'] = os.environ.get('DATABASE_URL') + '?sslmode=require'
+app.config['SQLALCHEMY_DATABASE_URI'] = os.environ.get('DATABASE_URL').replace('postgres', 'postgresql')
 
 import models
 models.db.init_app(app)
