c3e551aed55b902b539b8ebdf5bdd6fc5e604ec5
Peter Macaluso <moxious.manoeuvre@gmail.com>
2021-05-12 14:02:49 -0400

    sslmode

--- application.py
+++ application.py
@@ -6,7 +6,7 @@ app = Flask(__name__)
 import os
 from dotenv import load_dotenv
 load_dotenv()
-app.config['SQLALCHEMY_DATABASE_URI'] = os.environ.get('DATABASE_URL')
+app.config['SQLALCHEMY_DATABASE_URI'] = os.environ.get('DATABASE_URL') + '?sslmode=require'
 
 import models
 models.db.init_app(app)
